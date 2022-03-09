526413610f954c3906b7976eec5881825ba79ef6
tayloraharry <taylor.andrew.harry@gmail.com>
2022-03-08 18:41:38 -0600

    WIP

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,29 +1,29 @@
 import { useSelector } from "react-redux";
+import { RoomStatus } from "what-the-trivia-types";
 import { RootState } from "../../store";
 import NewGame from "../new-game";
 import Questions from "../questions";
 import Rules from "../rules";
+import Standings from "../standings";
 import "./index.css";
 
 const Main = () => {
-  const { room } = useSelector<RootState, RootState["roomReducer"]>(
+  const { room: { status } } = useSelector<RootState, RootState["roomReducer"]>(
     (state) => state.roomReducer
   );
+
+  const { Waiting, Rules : RulesStatus, Question, CurrentScore, FinalScore } = RoomStatus;
 
   return (
     <div
       className="main"
       style={{ backgroundImage: `url(images/title-background.jpg)` }}
     >
-      {room.started ? (
-        room.currentQuestion.number === 0 ? (
-          <Rules />
-        ) : (
-          <Questions />
-        )
-      ) : (
-        <NewGame />
-      )}
+      { status === Waiting && <NewGame/> }
+      { status === RulesStatus && <Rules /> }
+      { status === Question && <Questions/> }
+      { status === CurrentScore && <Standings/> }
+      { status === FinalScore && <h1>Final Score Component Needed</h1> }
     </div>
   );
 };
